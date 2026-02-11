ad258dbd24abd1a766995b6c4e79108f6d56fb5c
Belokopytov Alexandr <aleksandrbelokopytov2@gmail.com>
2026-02-11 17:56:11 +0300

    auto-light task completed

---
+++ get-led/auto-light.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+led = 26
+light = 6
+
+GPIO.setup(light, GPIO.IN)
+GPIO.setup(led, GPIO.OUT)
+
+while True:
+    GPIO.output(led, not GPIO.input(light))
